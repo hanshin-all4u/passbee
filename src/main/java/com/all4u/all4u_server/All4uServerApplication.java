b005adb6838b086944a5c70a58250901c3ff151f
crow000n <crow000n@gmail.com>
2025-08-27 17:06:05 +0900

    first commit

---
+++ src/main/java/com/all4u/all4u_server/All4uServerApplication.java
@@ -0,0 +1,13 @@
+package com.all4u.all4u_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class All4uServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(All4uServerApplication.class, args);
+	}
+
+}
