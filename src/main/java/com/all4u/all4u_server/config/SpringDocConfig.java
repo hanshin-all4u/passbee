0ba1f56df67f45660e74107ebdff5b542fbb85a6
crow000n <crow000n@gmail.com>
2025-09-30 17:36:07 +0900

    7 - add SpringDocConfig

---
+++ src/main/java/com/all4u/all4u_server/config/SpringDocConfig.java
@@ -0,0 +1,39 @@
+package com.all4u.all4u_server.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * SpringDoc API 문서 관련 설정
+ */
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("PassBee API Document")
+                .version("v0.0.1")
+                .description("PassBee 프로젝트의 API 명세서입니다.");
+
+        // API 문서에 인증 기능 추가
+        String jwtSchemeName = "JWT TOKEN";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("bearer")
+                        .bearerFormat("JWT")); // 토큰 형식 지정
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
